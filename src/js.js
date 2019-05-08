0c6ced069bdee3fb00648f8098cac17746078db6
Mariusz Buskiewicz <grajzesz@gmail.com>
2019-05-08 15:44:44 +0200

    oki

---
+++ src/js.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    function adding(e) {
+        $('ul').append(
+            '<li>' +
+                'Task Number' +
+                ' ' +
+                counter++ +
+                ': ' +
+                $('input[name=ListItem]').val() +
+                '</li>'
+        );
+    }
+    function error(e) {
+        $('ul').append(`<li>INPUT TASK</li>`);
+    }
+    let counter = 1;
+
+    $('.add').click(function(e) {
+        e.preventDefault();
+        if ($('input[name=ListItem]').val() === '') {
+            error();
+        } else {
+            adding();
+        }
+    });
+    $('input').keypress(function(e) {
+        if (e.which == 13) {
+            e.preventDefault();
+            if ($('input[name=ListItem]').val() === '') {
+                error();
+            } else {
+                adding();
+            }
+        }
+    });
+
+    $('.remove').click(function(e) {
+        e.preventDefault();
+        $('li')
+            .last()
+            .remove();
+    });
+    $('.clear').click(function(e) {
+        e.preventDefault();
+        counter = 1;
+        $('li').remove();
+    });
+});
